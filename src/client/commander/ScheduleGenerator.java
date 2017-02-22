e7daa39e3339608731f1a7baaf143bbbab1172ba
Chucheng Lin <398549982@qq.com>
2017-02-22 12:02:20 -0800

    Update ScheduleGenerator.java

--- src/client/commander/ScheduleGenerator.java
+++ src/client/commander/ScheduleGenerator.java
@@ -1,2 +1,43 @@
 package client.commander;
-public class ScheduleGenerator{}
+import client.app.obj.ScheduleEvent;
+import client.app.obj.User;
+
+import java.util.ArrayList;
+
+
+public class ScheduleGenerator{
+  private ArrayList<ScheduleEvent> events;
+  private int number_of_events;
+  private ArrayList<ArrayList<ScheduleEvent>> nodes;
+  
+  public ScheduleGenerator(ArrayList<ScheduleEvent> e, int number){
+    events = e;
+    number_of_events = number;
+    nodes = new ArrayList<ArrayList<ScheduleEvent>>();
+    for (int i=0; i<number; i++){
+      ArrayList<ScheduleEvent> nodeList = new ArrayList<ScheduleEvent>();
+      nodes.add(nodeList);
+    }
+  }
+  
+  public void sperate_events(){
+    
+    for(int i=0; i<this.number_of_events;i++){
+      ScheduleEvent s = events.get(0);
+      nodes.get(i).add(s);
+      this.events.remove(s);
+      Iterator<ScheduleEvent> iter = events.iterator();
+    	while (iter.hasNext()){
+    		ScheduleEvent ss = iter.next();
+     		if (s.getID() == ss.getID())
+    		{
+            nodes.get(i).add(ss);
+    	    	this.events.remove(ss);
+    		}
+    	}
+     }
+  }
+  
+  public void 
+ 
+}
