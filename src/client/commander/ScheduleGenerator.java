fc3215aba4bd4b153b47915ed9cca51ac99a118a
Chucheng Lin <398549982@qq.com>
2017-03-02 11:16:50 -0800

    Update ScheduleGenerator.java

--- src/client/commander/ScheduleGenerator.java
+++ src/client/commander/ScheduleGenerator.java
@@ -3,55 +3,89 @@ import client.app.obj.ScheduleEvent;
 import client.app.obj.User;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 
 public class ScheduleGenerator{
   private ArrayList<ScheduleEvent> events;
   private int number_of_events;
-  private ArrayList<ArrayList<ScheduleEvent>> nodes;
-  private ArrayList<ScheduleEvent> output;
   
   public ScheduleGenerator(ArrayList<ScheduleEvent> e, int number){
     events = e;
     number_of_events = number;
-    nodes = new ArrayList<ArrayList<ScheduleEvent>>();
-    for (int i=0; i<number; i++){
-      ArrayList<ScheduleEvent> nodeList = new ArrayList<ScheduleEvent>();
-      nodes.add(nodeList);
-    }
-    output = new ArrayList<ScheduleEvent>();
   }
   
-  public void sperate_events(){
-    
-    for(int i=0; i<this.number_of_events;i++){
-      ScheduleEvent s = events.get(0);
-      nodes.get(i).add(s);
-      this.events.remove(s);
-      Iterator<ScheduleEvent> iter = events.iterator();
-    	while (iter.hasNext()){
-    		ScheduleEvent ss = iter.next();
-     		if (s.getID() == ss.getID())
-    		{
-            nodes.get(i).add(ss);
-    	    	this.events.remove(ss);
-    		}
-    	}
-    }
-  }
   
-  public void creat_schedule(){
+  public ArrayList<ScheduleEvent> creat_schedule(){
     do{
+        ArrayList<ScheduleEvent> output = new ArrayList<ScheduleEvent>();
         for (int i=0; i<this.number_of_events; i++){
-            int a = this.nodes.get(i).size();
+            int a = events.size();
             Random random = new Random();
             int index = random.nextInt(a);
-            ScheduleEvent randomEvent = this.nodes.get(i).get(index);
-            this.ouput.add(randomEvent);
+            ScheduleEvent randomEvent = events.get(index);
+            ouput.add(randomEvent);
         }
-        int count = 0;
-
-      }while
-  } 
+    }while(check_ID(output==false) || check_timeblock(output==false)
+           return output;
+  }
+            
+  public boolean check_ID(ArrayList<ScheduleEvent> e){
+    ArrayList<ScheduleEvent> copy = new ArrayList<ScheduleEvent>();
+    copy = e;
+    do{
+        ScheduleEvent hold = copy.get(0);
+        copy.remove(0);
+        Iterator<ScheduleEvent> iter = copy.iterator();
+        while (iter.hasNext()){
+          ScheduleEvent ss = iter.next();
+          if(hold.getID() == ss.getID()){
+            return false;
+          }
+        }
+      }while(copy.size()!=0)
+      return true;
+  }
+           
+  public boolean check_timeblock(ArrayList<ScheduleEvent> e){
+    ArrayList<ScheduleEvent> copy = new ArrayList<ScheduleEvent>();
+    copy = e;
+    copy = e;
+    do{
+        ScheduleEvent hold = copy.get(0);
+        copy.remove(0);
+        Iterator<ScheduleEvent> iter = copy.iterator();
+        while (iter.hasNext()){
+          ScheduleEvent ss = iter.next();
+          if(hold.what_day() == ss.what_day()){
+            if (hold.when_to_start()> ss.when_to_start() && hold.when_to_start()<ss.when_to_end()){return false;}
+            if (ss.when_to_start()> hold.when_to_start() && ss.when_to_start()<hold.when_to_end()){return false;}
+          }
+        }
+      }while(copy.size()!=0)
+      return true;
+  }
  
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
 }
